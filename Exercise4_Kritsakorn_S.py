e4a97c592f833c1360718f34fbb906017b053df2
sweetthum <sweetthum@gmail.com>
2021-02-16 22:24:51 +0700

    Update Exercise4_Kritsakorn_S.py

--- Exercise4_Kritsakorn_S.py
+++ Exercise4_Kritsakorn_S.py
@@ -5,3 +5,6 @@ CP = 40
 
 print("--- Your Score ---")
 print("Foundation English :",FE)
+print("General Business :",GB)
+print("Introduction to Computer Systems :",ICS)
+print("Computer Programming :",CP)
